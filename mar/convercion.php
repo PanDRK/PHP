a0a2b71e0bf2dfe4b4496fa49293d6c9cdb11981
PanDRK <146790792+PanDRK@users.noreply.github.com>
2024-05-17 09:34:28 -0600

    Update convercion.php

--- mar/convercion.php
+++ mar/convercion.php
@@ -9,68 +9,83 @@
 </head>
 
 <body>
-    <div class="form">
-
+    <div class="forms">
         <?php
-        function blr($dinero)
+        function brl($dinero)
         {
-            return ($dinero / 3.25);
+            return ($dinero * 0.31);
         }
 
         function bob($dinero)
         {
-            return ($dinero / 2.38);
+            return ($dinero * 0.42);
         }
 
         function bzd($dinero)
         {
-            return ($dinero / 8.21);
+            return ($dinero * 0.12);
         }
 
         function eur($dinero)
         {
-            return ($dinero / 18.15);
+            return ($dinero * 0.055);
         }
 
         //bandera, imagen de la moneda, nombre, nombre de la moneda
 
         if (isset($_POST['tipo']) && isset($_POST['cant']) && isset($_POST['nombre'])) {
-            $dinero = $_POST['cant'];
+            $dinero = floatval($_POST['cant']);
             $name = $_POST['nombre'];
 
             switch ($_POST['tipo']) {
-                case 'Reales Brasileños':
-                    echo " <h1>Reales Bracileños</h1> <br><br>Bienvenido $name <br> 
-                            La cantidad de $$dinero MXN son: " . blr($dinero) . ' BLR.';
-                    echo '<img src="/recursos/img/brasil.jpg" alt="Bandera de Brasil">';
-                    echo '<img src="/recursos/img/brasil_mon.jpg" alt="Moneda brasileña">';
+                case 'Reales Brasileño':
+
                     break;
                 case 'Boliviano':
-                    echo "<h1>Bolivanos</h1> <br><br>Bienvenido $name <br> La 
-                            cantidad de $$dinero MXN son: " . bob($dinero) . ' BOB.';
-                    echo '<img src="/recursos/img/bol.jpg" alt="Bandera de Bolivia">';
-                    echo '<img src="/recursos/img/bol_mon.jpg" alt="Moneda de bolivia">';
+
                     break;
                 case 'Dolar Beliceño':
-                    echo "<h1>Dolar Beliseño</h1> <br><br>Bienvenido $name <br> La 
-                            cantidad de $$dinero MXN son: " . bzd($dinero) . ' BZD.';
-                    echo '<img src="/recursos/img/belice.png" alt="Bandera de Belice">';
-                    echo '<img src="/recursos/img/belice_mon.png" alt="Moneda beliceña">';
+
                     break;
-                case 'Euros':
-                    echo "<h1>Euros</h1> <br><br>Bienvenido $name <br> La cantidad de
-                            $$dinero MXN son: " . eur($dinero) . ' EUR.';
-                    echo '<img src="/recursos/img/bel.jpg" alt="Bandera de Belgica">';
-                    echo '<img src="/recursos/img/euro.jpg" alt="Bandera de">';
+                case 'Euro':
+                    echo '<div style="display: flex; ">
+                        <h1>Belgica - Euros </h1>
+                        <img src="recursos/img/bel.jpg" alt="Bandera de Belgica" style="margin-left: 10px; height: 50px; padding-top: 20px;">
+                        </div> <br><br>' .
+                        '<div style="display: flex; ">' .
+                        "<br>Bienvenido $name <br> La cantidad de $$dinero MXN son: €" . eur($dinero) . ' EUR.' .
+                        '<img src="recursos/img/euro.png" alt="Bandera de Belgica" style="margin-left: 10px; height: 50px; padding-top: 30px;"> <br>
+                        </div> <br><br>' . 'Este pais se encuentra en el continente Europeo.';
                     break;
             }
         } else {
             echo "<h1>Ingresa una opcion</h1> <br><br>";
         }
-
-
         ?>
     </div>
+<!-- 
+    echo '<div style="display: flex; ">
+                        <h1 style="font-size: 30px;">Brasil - Reale Brasileño</h1>
+                        <img src="recursos/img/brasil.png" alt="Bandera de Brasil" 
+                        style="margin-left: 10px; height: 50px; padding-top: 20px;">
+                        </div>' . "<br>Bienvenido $name <br> 
+                        La cantidad de $$dinero MXN son: " . brl($dinero) . ' BRL.';
+
+                    echo '<img src="recursos/img/bel.jpg" alt="Bandera de Belgica">';
+echo '<div style="display: flex; ">
+                        <h1>Bolivia - Boliviano</h1>
+                        <img src="recursos/img/bol.png" alt="Bandera de Bolivia" 
+                        style="margin-left: 10px; height: 50px; padding-top: 20px;">
+                        </div>' . "<br>Bienvenido $name <br> La 
+                        cantidad de $$dinero MXN son: " . bob($dinero) . ' BOB.';
+                    echo '<img src="recursos/img/bel.jpg" alt="Bandera de Belgica">';
+echo '<div style="display: flex; ">
+                        <h1 style="font-size: 30px;">Belice - Dolar Bliceño</h1>
+                        <img src="recursos/img/belice.png" alt="Bandera de Belice" 
+                        style="margin-left: 10px; height: 70px; padding-top: 05px;">
+                        </div>' . 
+                        "<br>Bienvenido $name <br> La cantidad de $$dinero MXN son: " . bzd($dinero) . ' BZD.';
+                    echo '<img src="recursos/img/bel.jpg" alt="Bandera de Belgica">'; -->
 
 </body>
 
