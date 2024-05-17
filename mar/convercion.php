e1306a7c899ab4598ce39403831cc0d43a89772d
PanDRK <146790792+PanDRK@users.noreply.github.com>
2024-05-17 00:08:16 -0600

    Add files via upload

--- mar/convercion.php
+++ mar/convercion.php
@@ -9,55 +9,68 @@
 </head>
 
 <body>
-    <center>
-        <div class="form">
-            <?php
-            function blr($dinero)
-            {
-                return ($dinero / 3.25);
+    <div class="form">
+
+        <?php
+        function blr($dinero)
+        {
+            return ($dinero / 3.25);
+        }
+
+        function bob($dinero)
+        {
+            return ($dinero / 2.38);
+        }
+
+        function bzd($dinero)
+        {
+            return ($dinero / 8.21);
+        }
+
+        function eur($dinero)
+        {
+            return ($dinero / 18.15);
+        }
+
+        //bandera, imagen de la moneda, nombre, nombre de la moneda
+
+        if (isset($_POST['tipo']) && isset($_POST['cant']) && isset($_POST['nombre'])) {
+            $dinero = $_POST['cant'];
+            $name = $_POST['nombre'];
+
+            switch ($_POST['tipo']) {
+                case 'Reales Brasileños':
+                    echo " <h1>Reales Bracileños</h1> <br><br>Bienvenido $name <br> 
+                            La cantidad de $$dinero MXN son: " . blr($dinero) . ' BLR.';
+                    echo '<img src="/recursos/img/brasil.jpg" alt="Bandera de Brasil">';
+                    echo '<img src="/recursos/img/brasil_mon.jpg" alt="Moneda brasileña">';
+                    break;
+                case 'Boliviano':
+                    echo "<h1>Bolivanos</h1> <br><br>Bienvenido $name <br> La 
+                            cantidad de $$dinero MXN son: " . bob($dinero) . ' BOB.';
+                    echo '<img src="/recursos/img/bol.jpg" alt="Bandera de Bolivia">';
+                    echo '<img src="/recursos/img/bol_mon.jpg" alt="Moneda de bolivia">';
+                    break;
+                case 'Dolar Beliceño':
+                    echo "<h1>Dolar Beliseño</h1> <br><br>Bienvenido $name <br> La 
+                            cantidad de $$dinero MXN son: " . bzd($dinero) . ' BZD.';
+                    echo '<img src="/recursos/img/belice.png" alt="Bandera de Belice">';
+                    echo '<img src="/recursos/img/belice_mon.png" alt="Moneda beliceña">';
+                    break;
+                case 'Euros':
+                    echo "<h1>Euros</h1> <br><br>Bienvenido $name <br> La cantidad de
+                            $$dinero MXN son: " . eur($dinero) . ' EUR.';
+                    echo '<img src="/recursos/img/bel.jpg" alt="Bandera de Belgica">';
+                    echo '<img src="/recursos/img/euro.jpg" alt="Bandera de">';
+                    break;
             }
-
-            function bob($dinero)
-            {
-                return ($dinero / 2.38);
-            }
-
-            function bzd($dinero)
-            {
-                return ($dinero / 8.21);
-            }
-
-            function eur($dinero)
-            {
-                return ($dinero / 18.15);
-            }
-            //bandera, imagen de la moneda, nombre, nombre de la moneda
-
-            if (isset($_POST['tipo']) & isset($_POST['cant'])) {
-                $dinero = $_POST['cant'];
-
-                switch ($_POST['tipo']) {
-                    case 'Reales Bracileños':
-                        echo " <h1>Reales Bracileños</h1> <br><br> La cantidad de $$dinero MXN son: " . blr($dinero) . ' BLR.';
-                        break;
-                    case 'Boliviano':
-                        echo "<h1>Bolivanos</h1> <br><br> La cantidad de $$dinero MXN son: " . bob($dinero) . ' BOB.';
-                        break;
-                    case 'Dolar Beliceño':
-                        echo "<h1>Dolar Beliseño</h1> <br><br> La cantidad de $$dinero MXN son: " . bzd($dinero) . ' BZD.';
-                        break;
-                    case 'Euros':
-                        echo "<h1>Euros</h1> <br><br> La cantidad de $$dinero MXN son: " . eur($dinero) . ' EUR.';
-                        break;
-                }
-            } else {
-                echo "<h1>Ingresa una opcion</h1> <br><br>";
-            }
+        } else {
+            echo "<h1>Ingresa una opcion</h1> <br><br>";
+        }
 
 
-            ?>
-        </div>
-    </center>
+        ?>
+    </div>
 
 </body>
 
--- mar/index.php
+++ mar/index.php
@@ -11,31 +11,63 @@
 </head>
 
 <body>
+	<div class="form">
+		<h1>Página de conversión de monedas</h1>
 
-	<center>
-		<div class="form">
-			<h1>Página de conversión de monedas</h1>
+		<form action="convercion.php" method="POST">
+			<label for="name">Ingrese su nombre:</label><br>
+			<input type="text" name="nombre" class="input"><br><br>
 
-			<form action="convercion.php" method="POST">
-				<label for="moneda_a_convertir">Ingresa la cantidad a convertir (MXN):</label><br>
-				<input type="number" name="cant"><br><br>
+			<label for="moneda_a_convertir">Ingresa la cantidad a convertir (MXN):</label><br>
+			<input type="number" name="cant" class="input"><br><br>
 
-				<label for="tipo">Brasil</label><br>
-				<input type="radio" name="tipo" value="Reales Bracileños">Reales Bracileños<br><br>
+			<div class="radio-group">
+				<input class="radio-input" name="tipo" id="radio1" value="Reales Brasileño" type="radio">
+				<label class="radio-label" for="radio1">
+					<span class="radio-inner-circle"></span>
+					Dolar Brasileño
+				</label>
 
-				<label for="tipo">Bolivia</label><br>
-				<input type="radio" name="tipo" value="Boliviano">Boliviano<br><br>
+				<input class="radio-input" name="tipo" id="radio2" value="Boliviano" type="radio">
+				<label class="radio-label" for="radio2">
+					<span class="radio-inner-circle"></span>
+					Boliviano
+				</label>
 
-				<label for="tipo">Belice</label><br>
-				<input type="radio" name="tipo" value="Dolar Beliceño">Dolar Beliceño<br><br>
+				<input class="radio-input" name="tipo" id="radio3" value="Dolar Beliceño" type="radio">
+				<label class="radio-label" for="radio3">
+					<span class="radio-inner-circle"></span>
+					Dolar Beliseño
+				</label>
 
-				<label for="tipo">Bélgica</label><br>
-				<input type="radio" name="tipo" value="Euros">Euros<br><br>
+				<input class="radio-input" name="tipo" id="radio4" value="Euro" type="radio">
+				<label class="radio-label" for="radio4">
+					<span class="radio-inner-circle"></span>
+					Euro
+				</label>
+			</div>
 
-				<button type="submit">Calcular</button>
-			</form>
-		</div>
-	</center>
+
+			<!-- <label for="tipo">Brasil</label><br>
+			<input type="radio" name="tipo" value="Reales Bracileños" class="Radio">Reales Bracileños<br><br>
+
+			<label for="tipo">Bolivia</label><br>
+			<input type="radio" name="tipo" value="Boliviano" class="Radio">Boliviano<br><br>
+
+			<label for="tipo">Belice</label><br>
+			<input type="radio" name="tipo" value="Dolar Beliceño" class="Radio">Dolar Beliceño<br><br>
+
+			<label for="tipo">Bélgica</label><br>
+			<input type="radio" name="tipo" value="Euros" class="Radio">Euros<br><br> -->
+
+			<button type="Submit" class="fancy" href="#">
+				<span class="top-key"></span>
+				<span class="text">Calcular</span>
+				<span class="bottom-key-1"></span>
+				<span class="bottom-key-2"></span>
+			</button>
+		</form>
+	</div>
 </body>
 
 </html>
